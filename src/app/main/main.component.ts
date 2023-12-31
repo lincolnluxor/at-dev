73e316402256c5aeb5b4592dd59fbe07cea52801
Adam Tate <lincolnluxor@gmail.com>
2023-12-30 17:58:29 -0800

    more style updates

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { NgIconComponent, provideIcons  } from '@ng-icons/core';
 import { bootstrapChevronDown } from '@ng-icons/bootstrap-icons';
@@ -10,11 +10,12 @@ import { HistoryComponent } from './history/history.component';
 import { OtherComponent } from './other/other.component';
 import { RunningComponent } from './running/running.component';
 import { ThanksComponent } from './thanks/thanks.component';
+import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [ThanksComponent, RunningComponent, OtherComponent, HistoryComponent, WhoamiComponent, HeroComponent, NgIconComponent, CommonModule],
+  imports: [NavigationComponent, ThanksComponent, RunningComponent, OtherComponent, HistoryComponent, WhoamiComponent, HeroComponent, NgIconComponent, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   providers: [
@@ -24,6 +25,18 @@ import { ThanksComponent } from './thanks/thanks.component';
 export class MainComponent implements OnInit {
   currentState = '';
   states: Object | undefined;
+  sticky: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const verticalOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (verticalOffset > window.innerHeight) {
+      this.sticky = true;
+    } else {
+      // Otherwise, set the sticky variable to false
+      this.sticky = false;
+    }
+  }
 
   constructor(
     private settingsService: SettingsService
