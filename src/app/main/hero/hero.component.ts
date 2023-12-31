cf793cbc28e25089179fa608407a8c3879b06818
Adam Tate <lincolnluxor@gmail.com>
2023-12-30 19:19:23 -0800

    more dotf

--- src/app/main/hero/hero.component.ts
+++ src/app/main/hero/hero.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { SettingsService } from '../../settings.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [],
+  providers: [DatePipe],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
-  title: string = "adamtate.dev";
-  constructor() {
+  time: string | null = '';
+  constructor(private datePipe: DatePipe) {}
 
+  ngOnInit(): void {
+    let now = new Date();
+    this.time = this.datePipe.transform(now, 'h:mm a', '-800', 'en-US');
+    this.getTime();
   }
 
+  getTime() {
+    setInterval(() => {
+      let now = new Date();
+      this.time = this.datePipe.transform(now, 'h:mm a', '-800', 'en-US');
+    }, 60000);
+  }
 }
